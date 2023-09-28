5626ac2e112af0e5c11774b94370ba7debf62799
Gustavo Do Espirito Santo <130726520+gustaeps2004@users.noreply.github.com>
2023-09-28 08:45:44 -0300

    Adicionando Mapeamento de entidades

---
+++ Versionamento/Versionamento.Application/Mappings/DomainToDtoMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Versionamento.Application.DTOs;
+using Versionamento.Domain.Entities;
+
+namespace Versionamento.Application.Mappings
+{
+    public class DomainToDtoMapping : Profile
+    {
+        public DomainToDtoMapping()
+        {
+            CreateMap<Usuarios, UsuariosDto>().ReverseMap();
+        }
+    }
+}
