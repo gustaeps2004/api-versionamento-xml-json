4364af310b43ade0e75fa3b20bcbc34e28c5b7c4
Gustavo Do Espirito Santo <130726520+gustaeps2004@users.noreply.github.com>
2023-09-28 08:42:06 -0300

    Adicionando camada de services

---
+++ Versionamento/Versionamento.Application/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using Versionamento.Application.DTOs;
+
+namespace Versionamento.Application.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<IEnumerable<UsuariosDto>> GetAll();
+        Task<UsuariosDto> GetByCodigo(Guid codigo);
+
+        Task CriarUsuario(UsuariosDto usuariosDto);
+        Task AtualizarUsuario(UsuariosDto usuariosDto, Guid codigo);
+        Task DeletarUsuario(Guid codigo);
+    }
+}
---
+++ Versionamento/Versionamento.Application/Services/UsuarioRepository.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Versionamento.Application.DTOs;
+using Versionamento.Application.Interfaces;
+
+namespace Versionamento.Application.Services
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IMapper _mapper;
+
+        public UsuarioRepository(IUsuarioRepository usuarioRepository, IMapper mapper)
+        {
+            _usuarioRepository = usuarioRepository;
+            _mapper = mapper;
+        }
+
+        public Task<IEnumerable<UsuariosDto>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuariosDto> GetByCodigo(Guid codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CriarUsuario(UsuariosDto usuariosDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarUsuario(UsuariosDto usuariosDto, Guid codigo)
+        {
+            throw new NotImplementedException();
+        }        
+
+        public Task DeletarUsuario(Guid codigo)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
