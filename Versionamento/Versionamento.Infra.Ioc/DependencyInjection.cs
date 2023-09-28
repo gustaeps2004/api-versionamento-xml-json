c983709778df2b8469e16ce76cb90faae5ddfe15
Gustavo Do Espirito Santo <130726520+gustaeps2004@users.noreply.github.com>
2023-09-28 09:10:08 -0300

    Adicionando infraestrutura

---
+++ Versionamento/Versionamento.Infra.Ioc/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Versionamento.Application.Interfaces;
+using Versionamento.Application.Mappings;
+using Versionamento.Application.Services;
+using Versionamento.Domain.Interfaces;
+using Versionamento.Infra.Data.Context;
+using Versionamento.Infra.Data.Repositories;
+
+namespace Versionamento.Infra.Ioc
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("VersionamentoAPI")));
+
+            services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+            services.AddTransient<IUsuarioServices, UsuarioServices>();
+
+            services.AddAutoMapper(typeof(DomainToDtoMapping));
+
+            return services;
+        }
+    }
+}
