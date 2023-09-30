d408338e507355b4ad2f336758eea1cbb7a05c25
gustaeps <130726520+gustaeps2004@users.noreply.github.com>
2023-09-29 23:17:30 -0300

    Classe ValidacaoBase e exception customizada implementada

---
+++ Versionamento/Versionamento.Application/Validation/Base/ValidacaoAbstrataBase.cs
@@ -0,0 +1,20 @@
+﻿using Versionamento.Application.Validation.ExceptionCustomizada;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Versionamento.Application.Validation.Base
+{
+    public class ValidacaoAbstrataBase<T> : AbstractValidator<T>
+    {
+        public Task<ValidationResult> Validator(ValidationContext<T> context, CancellationToken cancellationToken)
+        {
+            var validate = base.ValidateAsync(context, cancellationToken);
+            if (validate.Result.IsValid)
+                return validate;
+            else
+            {
+                throw new ApplicationExceptionValidation(validate.Result.ToString());
+            }
+        }
+    }
+}
